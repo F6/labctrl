328d5909ee88e0ca480910f371db6d9c69722e95
F6 <x@zzi.io>
2023-03-16 16:12:52 +0800

    update ziUHF ziutils library to new API

--- servers/lockin_and_boxcars/ziUHF_sync/server.bat
+++ servers/lockin_and_boxcars/ziUHF_sync/server.bat
@@ -1,6 +1,6 @@
 CALL C:\ProgramData\Anaconda3\condabin\conda.bat activate base
 
 set FLASK_APP=ziUHF_sync_server
-python -m flask run --host=0.0.0.0 --port=5060
+python -m flask run --host=127.0.0.1 --port=5060
 
 pause
--- servers/lockin_and_boxcars/ziUHF_sync/ziUHF_sync.py
+++ servers/lockin_and_boxcars/ziUHF_sync/ziUHF_sync.py
@@ -13,19 +13,20 @@ import time
 import numpy as np
 from threading import Thread
 
-import zhinst.utils
+# import zhinst.utils
+import zhinst.core
 
 from circular_buffer import CircularBuffer
 
 cfg = {
-    # "DeviceID": "dev2461",
-    "DeviceID": "dev2819",
+    "DeviceID": "dev2461",
+    # "DeviceID": "dev2819",
     "APILevel": 6,
     # "ServerHost": "localhost",
-    "ServerHost": "192.168.1.149",
+    "ServerHost": "127.0.0.1",
     "ServerPort": 8004,
-    # "SamplePath": "/dev2461/boxcars/0/sample"
-    "SamplePath": "/dev2819/boxcars/1/sample",
+    "SamplePath": "/dev2461/boxcars/0/sample"
+    # "SamplePath": "/dev2819/boxcars/1/sample",
     # "BackgroundSamplePath": "/dev2819/boxcars/1/sample"
 }
 
@@ -38,25 +39,28 @@ class ziUHF:
         self.daq = None
         self.device = None
         self.props = None
+        self.poll_length = 0.1  # [s]
+        self.poll_timeout = 500  # [ms]
+        self.poll_flags = 0
+        self.poll_return_flat_dict = True
         self.buffer = CircularBuffer(length=4096)
         self.init_session()
         self.sync_thread_running = True
         self.sync_thread = Thread(target=self.sync_task)
         self.sync_thread.start()
-        self.poll_length = 0.1  # [s]
-        self.poll_timeout = 500  # [ms]
-        self.poll_flags = 0
-        self.poll_return_flat_dict = True
+
 
     def init_session(self) -> None:
         # Call a zhinst utility function that returns:
         # - an API session `daq` in order to communicate with devices via the data server.
         # - the device ID string that specifies the device branch in the server's node hierarchy.
         # - the device's discovery properties.
-        (self.daq, self.device, self.props) = zhinst.utils.create_api_session(
-            cfg["DeviceID"], cfg["APILevel"], server_host=cfg["ServerHost"], server_port=cfg["ServerPort"]
-        )
-        zhinst.utils.api_server_version_check(self.daq)
+        # (self.daq, self.device, self.props) = zhinst.utils.create_api_session(
+        #     cfg["DeviceID"], cfg["APILevel"], server_host=cfg["ServerHost"], server_port=cfg["ServerPort"]
+        # )
+        self.daq = zhinst.core.ziDAQServer('localhost', 8004, 6)
+
+        # zhinst.utils.api_server_version_check(self.daq)
         self.daq.subscribe(cfg["SamplePath"])
         # self.daq.subscribe(cfg["BackgroundSamplePath"])
 
@@ -72,7 +76,7 @@ class ziUHF:
                 print("[ziUHF]: KeyError {}, {}, requred result not present in poll from DAQ, \
 probably misconfigured SamplePath or node not subscribed".format(e, e.args))
 
-    def get_new_data(self, sample_count: int = 1000, timeout: float = 30.0, keep_phase_period: int = 1):
+    def get_new_data(self, sample_count: int = 1000, timeout: float = 30.0, keep_phase_period: int = 2):
         """
         Returns an array containing new samples of required sample count size.
         A timeout must be set to prevent freezing forever, if the device is accidentally 
